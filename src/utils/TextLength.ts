d5a0d1ee9dd52f6b3dc750942f165fd7a424a7c2
Kaiseridze <kaiseidze@gmail.com>
2022-11-03 05:17:46 +0300

    fix bug with length

--- src/utils/TextLength.ts
+++ src/utils/TextLength.ts
@@ -1,5 +1,5 @@
 const TextLength = (text: string) => {
-  return text.length > 40 ? text.slice(0, 39) + "..." : text;
+  return text?.length > 40 ? text.slice(0, 39) + "..." : text;
 };
 
 export default TextLength;
